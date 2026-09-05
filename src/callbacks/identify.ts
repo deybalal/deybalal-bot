1efe7f9587ba8a2a850dc0f7f0cfdb3a0530aeeb
deybalal <itsowner@proton.me>
2026-09-06 00:36:57 +0330

    disabled link preview in 'identify' message

--- src/callbacks/identify.ts
+++ src/callbacks/identify.ts
@@ -4,6 +4,9 @@ export function registerIdentifyCallback(bot: Bot) {
   bot.callbackQuery("identify", async (ctx) => {
     const text = `برای شناسایی آهنگ، لطفاً بخشی از آهنگ را بصورت وویس (voice) بفرستید. \n\nحداقل 10 ثانیه از آهنگ و ترجیحا از بخشی که صدای خواننده معلوم باشه رو بفرستید.\n\nبه "دقت تشخیص" هم توجه داشته باشید و احتمال خطا داشتن رو هم درنظر بگیرید!\n\n برای تست کردن این قابلیت، میتونید پیش نمایش هایی که توی <a href="https://t.me/deybalalir">کانال دی بلال</a> هست رو فروارد کنید برای ربات.`;
 
-    await ctx.reply(text, { parse_mode: "HTML" });
+    await ctx.reply(text, {
+      parse_mode: "HTML",
+      link_preview_options: { is_disabled: true },
+    });
   });
 }
